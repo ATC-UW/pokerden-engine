d1fc04caecae141f4dd4a3c6bb6d49a0180a9c2c
kipiiler <hoanghohohaha@gmail.com>
2025-07-25 00:10:48 -0700

    update retry

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ NUM_ROUNDS = 6
 SERVER_SIM_WAIT_BETWEEN_GAMES = 0.5 # seconds, time to wait between games in simulation mode
 OUTPUT_GAME_RESULT_FILE = os.path.join(BASE_PATH, "game_result.log")
 OUTPUT_FILE_SIMULATION = os.path.join(BASE_PATH, "sim_result.log")
-RETRY_COUNT = 2
+RETRY_COUNT = 1
 
 # Server configuration
 HOST = 'localhost'
