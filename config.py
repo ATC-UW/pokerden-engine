744beb2e0f8eddf7a15af998a6a320eb024f1cba
kipiiler <hoanghohohaha@gmail.com>
2025-08-09 18:14:18 -0700

    pushing limit

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BASE_PATH = DOCKER_BASE_PATH if IS_DOCKER else LOCAL_BASE_PATH
 os.makedirs(BASE_PATH, exist_ok=True)
 
 NUM_ROUNDS = 6
-SERVER_SIM_WAIT_BETWEEN_GAMES = 0.05 # seconds, time to wait between games in simulation mode
+SERVER_SIM_WAIT_BETWEEN_GAMES = 0.01 # seconds, time to wait between games in simulation mode
 OUTPUT_GAME_RESULT_FILE = os.path.join(BASE_PATH, "game_result.log")
 OUTPUT_FILE_SIMULATION = os.path.join(BASE_PATH, "sim_result.log")
 RETRY_COUNT = 1
